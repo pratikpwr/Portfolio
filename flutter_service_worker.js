cd876b222a7d9f1a24ed28a3e341f64fb9dcd2e3
pratikpwr <pratiksatishpawar@gmail.com>
2021-01-11 17:45:04 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -66,8 +66,8 @@ const RESOURCES = {
 "favicon.png": "14d138606e1825524893c14af9f167e3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "e6df671ffcd102b5e60e2a1885938772",
-"/": "e6df671ffcd102b5e60e2a1885938772",
+"index.html": "c2f85749e5e3fb0629a01111f37b4cdc",
+"/": "c2f85749e5e3fb0629a01111f37b4cdc",
 "main.dart.js": "178b400c6ddfd106ecdea77d76f702b4",
 "manifest.json": "499b519cb5cbed3c5dc70a8d67208a32",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
